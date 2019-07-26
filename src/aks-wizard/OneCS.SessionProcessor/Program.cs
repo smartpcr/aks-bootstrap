75156997cc9ca91b3b697669c655e6a0371fef4a
xiaodoli <xiaodoli@microsoft.com>
2019-07-25 17:07:49 -0700

    "skeleton for api/web/job projs"

---
+++ src/aks-wizard/OneCS.SessionProcessor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OneCS.SessionProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
